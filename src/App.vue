dd298d60b1ed799c103f718fe3d44f81bd49d60e
unknown <pateldhwani59@gmail.com>
2020-12-10 19:43:48 -0800

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
+    <vue-menu></vue-menu>
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Hello World"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
+import VueMenu from './components/Menu.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    VueMenu
   }
 }
 </script>
---
+++ src/components/Menu.vue
@@ -0,0 +1,15 @@
+<template>
+    <h3> I Am The Menu Component</h3>
+
+</template>
+
+<script>
+
+export default{
+    name: 'VueMenu'
+}
+
+</script>
+
+<style lang="scss" scoped>
+</style>
